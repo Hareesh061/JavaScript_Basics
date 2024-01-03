6171d1bdc17b4bae58b5cb7ec669399e509c1159
Hareesh Naik <hareeshnaik836@gmail.com>
2024-01-03 15:38:06 +0530

    update

---
+++ conditional_statements.js
@@ -0,0 +1,14 @@
+let n1=4
+let n2=5
+let n3=9
+
+if(n1>n2){
+    console.log("n1 is greater");
+}
+else if(n1<n3 && n2<n3){
+    console.log("n3 greater");
+}
+
+else{
+    console.log("n2 is greater");
+}
