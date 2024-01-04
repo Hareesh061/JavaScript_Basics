dff9fbc6568a9af1f6994dd0da0d31ef83cd07af
Hareesh Naik <hareeshnaik836@gmail.com>
2024-01-04 12:55:25 +0530

    object file

---
+++ Object.js
@@ -0,0 +1,10 @@
+//what is object? it represents a real antity. or its a data structure consisting of key value pairs.
+let alian={
+    name: 'Harry',
+    tech: 'Reactjs',
+    experience: 'Fresher'
+
+};
+
+
+console.log(alian.tech);
