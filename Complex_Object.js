10f0426875f4d73e5f6691c6f3caf4de8b015099
Hareesh Naik <hareeshnaik836@gmail.com>
2024-01-04 13:43:11 +0530

    complex_object file

---
+++ Complex_Object.js
@@ -0,0 +1,24 @@
+//complex object and delete property
+
+
+
+let alien = {
+    name : 'hari',
+    tech : 'react.js',
+
+//object under another object 
+
+    big_alien : {
+        name : 'hareesh',
+        teck : 'nodejs'
+
+    }
+}
+
+
+//delete values 
+delete alien.name;
+
+
+console.log(alien.alien);
+
