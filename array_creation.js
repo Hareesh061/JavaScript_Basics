a755b768e3c3628b76fb8436ea999ca82fd2703e
Hareesh Naik <hareeshnaik836@gmail.com>
2024-01-05 14:46:23 +0530

    added

---
+++ array_creation.js
@@ -0,0 +1,4 @@
+let A = [3,5,6];
+
+// let arr = new Array();
+console.log(A);
---
+++ constructor.js
@@ -0,0 +1,10 @@
+//constructor function
+function Alien(name,tech){
+    this.name=name;
+    this.tech=tech;
+
+}
+
+let alien1= new Alien('arun','js');
+
+console.log(alien1);
