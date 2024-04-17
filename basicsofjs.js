969f54cc6888fc4625255a879acbb5f8ab881d2d
Hareesh Naik <hareeshnaik836@gmail.com>
2024-04-17 20:48:41 +0530

    added

---
+++ basicsofjs.js
@@ -0,0 +1,16 @@
+// javascript basics questions
+
+
+// js intro and clint server
+
+// datatypes -> 6 primitive(number, boolean, string, null, undefined, symbol) and 1 non primitive(object)  , variables -> const, let , var,  scope-> block, functional , global
+
+
+//intro of data types
+
+let tem= 10;
+
+tem= true;
+typeof(tem);
+
+console.log(tem);
