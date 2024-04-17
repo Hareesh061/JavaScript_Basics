c46f097689a01810290770c210e9710c2edd4001
Hareesh Naik <hareeshnaik836@gmail.com>
2024-04-17 21:39:06 +0530

    added

--- basicsofjs.js
+++ basicsofjs.js
@@ -14,3 +14,27 @@ tem= true;
 typeof(tem);
 
 console.log(tem);
+
+
+// methods
+
+//1.tostring();
+//2.toFixed( ) :
+//3.toFixed( ) :
+
+
+
+//Variables to Numbers 
+
+//Number( ) : Converts from a variable to number
+
+//parseInt( ) : Parses its argument and returns an integer
+
+//parseFloat( ) : Parses its argument and returns a floating point number
+
+
+
+// taking input in js
+
+//5. conditionals
+
